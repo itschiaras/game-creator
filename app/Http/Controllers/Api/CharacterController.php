14b95f82d99fe814fd045428d92ea885789ce947
Gaetano26 <gaetano.spampinato@outlook.it>
2023-06-15 16:57:40 +0200

    Api completed

---
+++ app/Http/Controllers/Api/CharacterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Character;
+
+class CharacterController extends Controller
+{
+    public function index()
+    {
+        $character = Character::with('type')->paginate(5);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Ok',
+            'results' => $character
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $character = Character::with('type', 'items')->where('id', $id)->first();
+         dd($character);
+        if ($character) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Ok',
+                'results' => $character
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'results' => 'Product not found !'
+            ], 404);
+        }
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CharacterController;
+use App\Models\Character;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/characters', [CharacterController::class, 'index']);
+Route::get('/characters/{id}', [CharacterController::class, 'show']);
