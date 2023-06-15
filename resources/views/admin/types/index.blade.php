ef70c2a5d257d8576cad43da65fbe139890017b3
Gaetano26 <gaetano.spampinato@outlook.it>
2023-06-15 15:16:24 +0200

    finish index show types

---
+++ resources/views/admin/types/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Types</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Tools</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($types as $type)
+                <tr>
+                    <th scope="row">{{ $type->id }}</th>
+                    <td>{{ $type->name }}</td>
+                    <td>{{ $type->created_at }}</td>
+                    <td>
+                        <a href="{{ route('admin.types.show', $type->slug) }}">Show</a>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+@endsection
